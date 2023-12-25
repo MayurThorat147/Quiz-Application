c3c7750037487ba982a16d9596914dd397eb19b9
MayurThorat147 <114915412+MayurThorat147@users.noreply.github.com>
2023-12-25 14:01:57 +0530

    Add files via upload

---
+++ quiz1.py
@@ -0,0 +1,244 @@
+quiz_data = {
+    "C++": [
+        {
+            "question": " Who invented C++?",
+            "options": ["Dennis Ritchie", "Ken Thompson", "Brian Kernighan", "Bjarne Stroustrup"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following is the correct syntax of including a user defined header files in C++?",
+            "options": ["#include [userdefined]", "#include “userdefined”", "#include <userdefined.h>", "#include <userdefined>"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "What is C++?",
+            "options": ["C++ is an object oriented programming language", "C++ is a procedural programming language", "C++ supports both procedural and object oriented programming language", "C++ is a functional programming language"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which of the following is used for comments in C++?",
+            "options": ["/* comment */", "// comment */", "// comment", "both // comment or /* comment */"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following approach is used by C++?",
+            "options": ["Left-right", "Right-left", "Bottom-up", "Top-down"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which of the following type is provided by C++ but not C?",
+            "options": ["double", "float", "int", "bool"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "By default, all the files in C++ are opened in _________ mode?",
+            "options": ["Binary", "VTC", "Text", "ISCII"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which is more effective while calling the C++ functions?",
+            "options": ["call by object", "call by pointer", "call by value", "call by reference"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which of the following is used to terminate the function declaration in C++?",
+            "options": [";", "]", ")", ":"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "Which keyword is used to define the macros in c++?",
+            "options": ["#macro", "#define", "macro", "define"],
+            "correct_answer": 1,
+        },
+    ],
+    "JAVA": [
+        {
+            "question": "Who invented Java Programming?",
+            "options": ["Guido van Rossum", "James Gosling", "Dennis Ritchie", "Bjarne Stroustrup"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "Which component is used to compile, debug and execute the java programs?",
+            "options": ["JRE", "JIT", "JDK", "JVM"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which one of the following is not a Java feature?",
+            "options": ["Object-oriented", "Use of pointers", "Portable", "Dynamic and Extensible"],
+            "correct_answer": 1,
+        },    
+        {
+            "question": "Which of these cannot be used for a variable name in Java?",
+            "options": ["identifier & keyword", "identifier", "identifier", "none of the mentioned"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "What is the extension of java code files?",
+            "options": ["(.js)", "(.txt)", "(.class)", "(.java)"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following is not an OOPS concept in Java?",
+            "options": ["Polymorphism", "Inheritance", "Compilation", "Encapsulation"],
+            "correct_answer": 2,
+        },
+        {
+            "question": " What is not the use of “this” keyword in Java?",
+            "options": ["Referring to the instance variable when a local variable has the same name", "Passing itself to the method of the same class", "Passing itself to another method", "Calling another constructor in constructor chaining"],
+            "correct_answer": 1,
+        },
+        {
+            "question": " Which of the following is a type of polymorphism in Java Programming?",
+            "options": ["Multiple polymorphism", "Compile time polymorphism", "Multilevel polymorphism", "Execution time polymorphism"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "What is Truncation in Java?",
+            "options": ["Floating-point value assigned to a Floating type","Floating-point value assigned to an integer type", "Integer value assigned to floating type", "Integer value assigned to floating type"],
+            "correct_answer": 1,
+        },
+        {
+            "question": " Which exception is thrown when java is out of memory?",
+            "options": ["MemoryError", "OutOfMemoryError", "MemoryOutOfBoundsException", "MemoryFullException"],
+            "correct_answer": 1,
+        },    
+   ],
+    "Python": [
+        {
+            "question": "Who developed Python Programming Language?",
+            "options": ["Wick van Rossum", "Rasmus Lerdorf", "Guido van Rossum", "Niene Stom"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Which type of Programming does Python support?",
+            "options": ["Object-oriented programming", "Structured programming", "Functional programming", "All of the mentioned"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Is Python case sensitive when dealing with identifiers?",
+            "options": ["No", "Yes", "Machine Dependent", "None of the Mentioned"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "Which of the following is the correct extension of the Python file?",
+            "options": ["(.python)", "(.pl)", "(.py)", "(.p)"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "Is Python code compiled or interpreted?",
+            "options": ["Python code is both compiled and interpreted", "Python code is neither compiled nor interpreted", "Python code is only compiled", "Python code is only interpreted"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "All keywords in Python are in _________?",
+            "options": ["Capitalized", "lower case", "UPPER CASE", "None of the mentioned"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following is used to define a block of code in Python language?",
+            "options": ["Indentation", "Key", "Brackets", "All of the mentioned"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "Which keyword is used for function in Python language?",
+            "options": ["Function", "def", "Fun", "Define"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "Which of the following character is used to give single-line comments in Python?",
+            "options": ["//", "#", "!", "/*"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "What does pip stand for python?",
+            "options": ["Pip Installs Python", "Pip Installs Packages", "Preferred Installer Program", "All of the mentioned"],
+            "correct_answer": 2,
+        },
+    ],
+    "SQL": [
+        {
+            "question": "Which SQL function is used to count the number of rows in a SQL query'?",
+            "options": ["COUNT()", "NUMBER()", "SUM()", "COUNT(*)"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following SQL clauses is used to DELETE tuples from a database table?",
+            "options": ["DELETE", "REMOVE", "DROP", "CLEAR"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "___________removes all rows from a table without logging the individual row deletions.'?",
+            "options": ["DELETE", "REMOVE", "DROP", "TRUNCATE"],
+            "correct_answer": 3,
+        },
+        {
+            "question": "Which of the following is not a DDL command'?",
+            "options": ["UPDATE", "TRUNCATE", "ALTER", "NONE OF THE MENTIONED"],
+            "correct_answer": 0,
+        },
+        {
+            "question": " ________________ is not a category of SQL command.'?",
+            "options": ["TCL", "SCL", "DCL", "DDL"],
+            "correct_answer": 1,
+        },
+        {
+            "question": "With SQL, how do you select all the records from a table named “Persons” where the value of the column “FirstName” ends with an “a”?",
+            "options": ["SELECT * FROM Persons WHERE FirstName=’a’", "SELECT * FROM Persons WHERE FirstName LIKE ‘a%’", "SELECT * FROM Persons WHERE FirstName LIKE ‘%a’", "SELECT * FROM Persons WHERE FirstName=’%a%’"],
+            "correct_answer": 2,
+        },
+        {
+            "question": "With SQL, how can you return the number of not null records in the “Persons” table'?",
+            "options": ["SELECT COUNT() FROM Persons", "SELECT COLUMNS() FROM Persons", "SELECT COLUMNS(*) FROM Persons", "SELECT COUNT(*) FROM Persons"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "The UNION SQL clause can be used with _____________'?",
+            "options": ["SELECT clause only", "DELETE and UPDATE clauses", "UPDATE clause only", "All of the mentioned"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "Which SQL keyword is used to sort the result-set?",
+            "options": ["ORDER BY", "SORT", "ORDER", "SORT BY"],
+            "correct_answer": 0,
+        },
+        {
+            "question": "Which of the following command makes the updates performed by the transaction permanent in the database?",
+            "options": ["ROLLBACK", "COMMIT", "TRUNCATE", "DELETE"],
+            "correct_answer": 1,
+        },
+    ],
+}
+
+def display_question(question_data):
+    print(question_data["question"])
+    for i, option in enumerate(question_data["options"]):
+        print(f"{i + 1}. {option}")
+    user_choice = int(input("Enter your choice (1, 2, 3, 4): ")) - 1
+    
+    if user_choice == question_data["correct_answer"]:
+        print("⭐Correct!⭐\n")
+        return True
+    else:
+        print(f"This is not the correct answer. The correct answer is: {question_data['options'][question_data['correct_answer']]}\n")
+        return False
+
+def main():
+    print("Welcome to Programming Quiz Application!")
+
+    for subject in quiz_data.keys():
+        print(f"{subject}")
+    selected_subject = input("Enter the subject you want to participate in: ")
+    if selected_subject in quiz_data:
+        print(f"\n--- {selected_subject} Section ---")
+        questions = quiz_data[selected_subject]
+        score = 0
+        for index, question in enumerate(questions):
+            print(f"\nQuestion {index + 1}:")
+            if display_question(question):
+                score += 1
+        print(f"You scored {score}/{len(questions)} in the {subject} section.\n")
+    else:
+        print("Invalid subject selection.")
+        
+if __name__ == "__main__":
+    main()
